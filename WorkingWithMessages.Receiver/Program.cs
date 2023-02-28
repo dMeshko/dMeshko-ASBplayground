7fff29e046363e78a7df8d2178ebba760ed58619
Darko Meshkovski <darko.meshkovski@wsa.com>
2023-02-28 16:55:56 +0100

    added batch sending, processor receiving and duplicate detection to messages demo

---
+++ WorkingWithMessages.Receiver/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json;
+using Azure.Messaging.ServiceBus;
+using WorkingWithMessages.Common;
+
+var connectionString = "Endpoint=sb://simplemessaging-sb-demo-students.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Z8CssayAWC1m9CO1jbjsS6NpjBUWK3vJA+ASbA3Plyk=";
+var queueName = "withmessages";
+
+var client = new ServiceBusClient(connectionString, new ServiceBusClientOptions
+{
+    TransportType = ServiceBusTransportType.AmqpWebSockets
+});
+
+var processor = client.CreateProcessor(queueName, new ServiceBusProcessorOptions
+{
+    AutoCompleteMessages = true,
+    MaxConcurrentCalls = 5
+});
+
+processor.ProcessMessageAsync += MessageHandler;
+processor.ProcessErrorAsync += ErrorHandler;
+
+processor.StartProcessingAsync();
+Console.ReadKey();
+processor.StopProcessingAsync();
+processor.CloseAsync();
+
+async Task MessageHandler(ProcessMessageEventArgs args)
+{
+    var message = args.Message;
+    if (string.IsNullOrWhiteSpace(message.ContentType))
+    {
+        Console.WriteLine(message.Body);
+        return;
+    }
+
+    var deserializedMessage = JsonSerializer.Deserialize<Person>(message.Body);
+    Console.WriteLine($"Person: {deserializedMessage.FirstName} {deserializedMessage.LastName}");
+}
+
+async Task ErrorHandler(ProcessErrorEventArgs args)
+{
+    Console.WriteLine("BUMMER!");
+}
---
+++ WorkingWithMessages.Sender/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json;
+using Azure.Messaging.ServiceBus;
+using Azure.Messaging.ServiceBus.Administration;
+using WorkingWithMessages.Common;
+
+var connectionString = "Endpoint=sb://simplemessaging-sb-demo-students.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Z8CssayAWC1m9CO1jbjsS6NpjBUWK3vJA+ASbA3Plyk=";
+var queueName = "withmessages";
+
+var adminClient = new ServiceBusAdministrationClient(connectionString);
+if (!await adminClient.QueueExistsAsync(queueName))
+{
+    await adminClient.CreateQueueAsync(new CreateQueueOptions(queueName)
+    {
+        RequiresDuplicateDetection = true,
+        DuplicateDetectionHistoryTimeWindow = TimeSpan.FromMinutes(1)
+    });
+}
+
+var client = new ServiceBusClient(connectionString, new ServiceBusClientOptions
+{
+    TransportType = ServiceBusTransportType.AmqpWebSockets
+});
+
+var sender = client.CreateSender(queueName);
+var batchMessage = await sender.CreateMessageBatchAsync();
+
+batchMessage.TryAddMessage(new ServiceBusMessage("hello")
+{
+    MessageId = "1"
+});
+batchMessage.TryAddMessage(new ServiceBusMessage("world")
+{
+    MessageId = "2"
+});
+
+// person message
+var person = new Person
+{
+    FirstName = "Darko",
+    LastName = "Meshkovski"
+};
+var serializedPerson = JsonSerializer.Serialize(person);
+var personMessage = new ServiceBusMessage(serializedPerson)
+{
+    MessageId = "3",
+    ContentType = typeof(Person).AssemblyQualifiedName
+};
+batchMessage.TryAddMessage(personMessage);
+
+await sender.SendMessagesAsync(batchMessage);
+await sender.CloseAsync();
